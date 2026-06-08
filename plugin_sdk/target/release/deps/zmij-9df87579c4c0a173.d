5fb45586712c368887c813f3b294beec4ccc6a6d
Jerrard Smith <jerrard.smith@southatlantictech.com>
2026-06-08 02:20:50 -0400

    chore: untrack Rust build artifacts

--- plugin_sdk/target/release/deps/zmij-9df87579c4c0a173.d
+++
@@ -1,9 +0,0 @@
-/home/user/github/rust-wasm-nyno-sdk/plugin_sdk/target/release/deps/zmij-9df87579c4c0a173.d: /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.21/src/lib.rs /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.21/src/stdarch_x86.rs /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.21/src/traits.rs
-
-/home/user/github/rust-wasm-nyno-sdk/plugin_sdk/target/release/deps/libzmij-9df87579c4c0a173.rlib: /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.21/src/lib.rs /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.21/src/stdarch_x86.rs /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.21/src/traits.rs
-
-/home/user/github/rust-wasm-nyno-sdk/plugin_sdk/target/release/deps/libzmij-9df87579c4c0a173.rmeta: /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.21/src/lib.rs /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.21/src/stdarch_x86.rs /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.21/src/traits.rs
-
-/home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.21/src/lib.rs:
-/home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.21/src/stdarch_x86.rs:
-/home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.21/src/traits.rs:
